b84f536fa729f5d084d4861b8c70773d864ce325
Guru Karthikeyan <gurukartkn@gmail.com>
2022-08-03 11:18:07 +0530

    first commit

---
+++ components/index.js
@@ -0,0 +1,7 @@
+export { default as Hero } from "./Hero";
+export { default as Header } from "./Header";
+export { default as Footer } from "./Footer";
+export { default as Facilities } from "./Facilities";
+export { default as Leader } from "./Leader";
+export { default as Benefits } from "./Benefits";
+export { default as Cta } from "./Cta";
